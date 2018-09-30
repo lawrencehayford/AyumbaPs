a524281890f44130e9806e5847d6ea04871ee207
Admin <admin@Mac-Admin.local>
2018-09-30 20:42:26 +0000

    Changes made on table view searching

--- Ayumba/views/ExploreViewController.swift
+++ Ayumba/views/ExploreViewController.swift
@@ -1,25 +1,46 @@
 
 import UIKit
 
-class ExploreViewController: UIViewController , UITableViewDataSource, UITableViewDelegate{
+class ExploreViewController: UIViewController , UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate{
     
-    let services = ["capentery","cleaning","plumbing","massage","makeup","painting","trainer"]
+    
+    @IBOutlet weak var table: UITableView!
+    @IBOutlet weak var SearchBar: UISearchBar!
+    var services = ["capentery","cleaning","plumbing","massage","makeup","painting","trainer"]
+    var filteredData: [String]!
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        filteredData = searchText.isEmpty ? services : services.filter { (item: String) -> Bool in
+            // If dataItem matches the searchText, return true to include it
+            return item.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+        }
+        
+        table.reloadData()
+        
+    }
+    
+    
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return (self.services.count)
+       
+        return filteredData.count
+       
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ServicesTableView
-        cell.ServiceImage.image = UIImage(named: (self.services[indexPath.row] + ".jpg"))
-        cell.ServiceName.text = self.services[indexPath.row]
-        return (cell)
+        
+            cell.ServiceImage.image = UIImage(named: (self.filteredData[indexPath.row] + ".jpg"))
+            cell.ServiceName.text = self.filteredData[indexPath.row].uppercased()
+        
+       return cell
     }
+    
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+         filteredData = services
         // Do any additional setup after loading the view.
     }
 
@@ -28,6 +49,25 @@ class ExploreViewController: UIViewController , UITableViewDataSource, UITableVi
         // Dispose of any resources that can be recreated.
     }
     
-
-
+    
+    @IBAction func OnSegmentSelected(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            print ("0 selected")
+            services = ["capentery","cleaning","plumbing","massage","makeup","painting","trainer"]
+            filteredData = services
+            table.reloadData()
+            break
+        case 1:
+            print ("1 selected")
+            services = ["capentery","cleaning"]
+            filteredData = services
+            table.reloadData()
+            break
+        default:
+            break
+           
+        }
+    }
+    
 }
